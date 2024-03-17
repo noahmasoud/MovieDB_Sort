2938b1caefa7cfb9747a8992e3c8947f230a62cd
Noah Masoud <114415606+nomasoud@users.noreply.github.com>
2024-03-17 18:34:59 -0500

    changes for parse file

--- fetch_n_print_movies.py
+++ fetch_n_print_movies.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
-TMDB_TRENDING_PATH = 'trending/movie/week'
+# Update to fetch weekly trending TV shows
+TMDB_TRENDING_PATH = 'trending/tv/week'
 TMDB_SEARCH_API_REQUEST = f'https://api.themoviedb.org/3/{TMDB_TRENDING_PATH}?'
 
-#def get_top_10_weekly_trending_movies():
+# def get_top_10_weekly_trending_tv_shows():
 response = requests.get(
     TMDB_SEARCH_API_REQUEST,
     params={
@@ -25,5 +26,30 @@ print(json_data)
 pretty_json_data = json.dumps(json_data, indent=4, sort_keys=True)
 print(pretty_json_data)
 
-weekly_trending_movie_object = json_data
-# Add Parsing Code Here
+weekly_trending_tv_show_object = json_data
+
+
+def print_data(data):
+    # Limit to the top 10 results
+    results = data.get('results', [])[:10]
+    for item in results:
+        print(
+            f"Title: {item.get('name', '')}, Popularity: {item.get('popularity', '')}, Vote Count: {item.get('vote_count', '')}, Vote Average: {item.get('vote_average', '')}"
+        )
+
+
+def print_sorted_data(data):
+    sorted_data = sorted(data.get('results', []),
+                         key=lambda x: x.get('vote_average', 0), reverse=True)[:10]  # Limit to top 10 sorted results
+    for item in sorted_data:
+        print(
+            f"Title: {item.get('name', '')}, Vote Average: {item.get('vote_average')}"
+        )
+
+
+print("\nData with Details:")
+print_data(weekly_trending_tv_show_object)
+
+# sorted data by vote average
+print("\nSorted Data by Vote Average:")
+print_sorted_data(weekly_trending_tv_show_object)
